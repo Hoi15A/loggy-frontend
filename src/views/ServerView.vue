57e939b508b1b69903f8bc268a6382027990c1d2
pedernin <pedernin@students.zhaw.ch>
2021-03-06 16:16:15 +0100

    Deleted about.vue and added ServerView instead

---
+++ src/views/ServerView.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-row>
+    <v-col cols="2">
+      <v-sheet rounded="lg">
+        <v-list color="transparent">
+          <v-list-item v-for="item in listItem" :key="item" link>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ item }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider class="my-2"></v-divider>
+
+          <v-list-item
+            link
+            color="grey lighten-4"
+          >
+            <v-list-item-content>
+              <v-list-item-title @click="changeGridView()">
+                Refresh
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-sheet>
+    </v-col>
+
+    <v-col>
+      <v-sheet v-if="this.gridView" min-height="70vh" rounded="lg">
+        <LogGrid/>
+      </v-sheet>
+      <v-sheet v-if="!this.gridView" min-height="70vh" rounded="lg">
+        <LogFile/>
+      </v-sheet>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import LogFile from "@/components/LogFile.vue";
+import LogGrid from "@/components/LogGrid.vue";
+
+
+
+export default Vue.extend({
+  name: "ServerView",
+
+  components: {
+    LogFile,LogGrid,
+  },
+
+  data: () => ({
+    gridView: true,
+    links: [
+      "Feature 1",
+      "Feature 2",
+      "Feature 3",
+      "Feature 4",
+    ],
+    listItem: [
+      "Live Logs",
+      "Errors",
+      "Archive",
+    ]
+  }),
+  methods: {
+    changeGridView: async function () {
+      this.gridView = !this.gridView;
+    }
+  },
+});
+</script>
