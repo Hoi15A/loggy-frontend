8f3820d9746770484734a8a09f9b155c30b43d98
pedernin <pedernin@students.zhaw.ch>
2021-03-22 19:39:19 +0100

    poc icons with mdi-font

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,4 +4,16 @@ import Vuetify from "vuetify/lib/framework";
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'mdi', // default
+        values: {
+            product: 'mdi-dropbox',
+            support: 'mdi-lifebuoy',
+            steam: 'mdi-steam-box',
+            pc: 'mdi-desktop-classic',
+            xbox: 'mdi-xbox',
+            playstation: 'mdi-playstation',
+            switch: 'mdi-nintendo-switch',
+        },
+    },
 });
