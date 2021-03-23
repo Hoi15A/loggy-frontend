9eca08e8f0ac5eb872a220e2e904ae2f36608b47
pedernin <pedernin@students.zhaw.ch>
2021-03-23 16:25:14 +0100

    added fetchServers #15

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,13 @@
-export class Api {
+export default class Api {
 
     static async makeSampleCall() {
         return await fetch("https://zhaw.neat.moe/api/query/sample");
     }
+
+    async fetchServers() {
+        const res = await fetch("https://zhaw.neat.moe/api/service/all");
+        console.log(res);
+        return res.json();
+    }
+
 }
