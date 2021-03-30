6381f9806ad1727a857ec6f7aaed5509b55e7309
schwaal3 <schwaal3@students.zhaw.ch>
2021-03-30 11:40:03 +0200

    Adjusted serviceApi.ts to properly check response value

--- src/api/serviceApi.ts
+++ src/api/serviceApi.ts
@@ -22,16 +22,14 @@ export default class ServiceApi extends Api {
         if(!res.ok) throw new Error(`Unable to delete job with id: ${id}`);
     }
 
-    static async addNewService(jsonService: { image: string; logConfigName: string; logDirectory: string; name: string; description: string; logServiceLocation: string }) {
+    static async addNewService(jsonService: { image: string; logConfig: string; logDirectory: string; name: string; description: string; location: number }) {
         const res = await fetch(`${Api.getApiBaseUrl()}/service/`, {method: "POST",
             headers: {
                 "Content-Type": "application/json;charset=utf-8"
             },
             body: JSON.stringify(jsonService),
         });
-        if (res.ok) {
-            return res.json();
-        } else {
+        if (!res.ok) {
             throw new Error(`Unable to create Service with Attributes:${JSON.stringify(jsonService)}`);
         }
     }
