1a4d96000aeb60df0900630aaa62a9367cf7aa77
schwaal3 <schwaal3@students.zhaw.ch>
2021-04-15 19:58:45 +0200

    added mock column component

---
+++ src/components/configViewComponents/ConfigColoumTestComponent.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-col>
+    <v-row>
+      <v-toolbar flat>
+        <v-row>
+          <v-col>
+            <v-checkbox v-model="checkBoxEdit" :label="'Reorder'">
+            </v-checkbox>
+          </v-col>
+          <v-col>
+            <v-btn v-if="checkBoxEdit">
+              Save
+            </v-btn>
+          </v-col>
+          <v-col>
+
+          </v-col>
+        </v-row>
+      </v-toolbar>
+    </v-row>
+    <v-row>
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+          <tr>
+            <th class="text-left">
+              Name
+            </th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="item in columns" :key="item.name">
+            <td>{{ item.name }}</td>
+          </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-row>
+  </v-col>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "ConfigColoumTestComponent",
+
+  data () {
+    return {
+      checkBoxEdit: false as boolean,
+      columns: [
+        {
+          name: "Frozen Yogurt",
+        },
+        {
+          name: "Ice cream sandwich",
+        },
+        {
+          name: "Eclair",
+        },
+      ],
+    };
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
--- src/components/configViewComponents/ConfigsExpansionPanel.vue
+++ src/components/configViewComponents/ConfigsExpansionPanel.vue
@@ -1,17 +1,17 @@
 <template>
-  <v-row>
-    <v-expansion-panels popout>
-      <v-expansion-panel v-for="(config) in configArray" v-bind:key="config.name">
-        <v-expansion-panel-header>
-          {{ config.name}}
-        </v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <ConfigEditor>
-          </ConfigEditor>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
-  </v-row>
+  <v-expansion-panels popout focusable multiple>
+    <v-expansion-panel v-for="(config) in configArray" v-bind:key="config.name">
+      <v-expansion-panel-header class="font-weight-medium" align="center">
+        {{ config.name}}
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <v-toolbar flat>
+        </v-toolbar>
+        <ConfigColoumTestComponent/>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+
 </template>
 
 
@@ -21,21 +21,25 @@
 import Vue from "vue";
 import {Config} from "@/models/Config";
 import ServiceApi from "@/api/serviceApi";
-import ConfigEditor from "@/components/ConfigEditor.vue";
+import ConfigColoumTestComponent from "@/components/configViewComponents/ConfigColoumTestComponent.vue";
 
 export default Vue.extend( {
   name: "ConfigsExpansionPanel",
   data: () => {
     return {
+      checkBoxEdit: false as boolean,
       configArray: [] as Config[],
     };
   },
 
   components: {
-    ConfigEditor
+    ConfigColoumTestComponent
   },
 
   methods: {
+    test() {
+      console.log("hallo");
+    },
 
     loadConfigs: async function() {
       console.log("loading configs");
