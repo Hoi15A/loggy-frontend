86a3a15b203d920ccf39ea27ce2266a467b155cc
pedernin <pedernin@students.zhaw.ch>
2021-04-15 11:40:29 +0200

    added createNewColumn and removeColumnById to columnApi.ts #12

--- src/api/columnCompApi.ts
+++ src/api/columnCompApi.ts
@@ -14,4 +14,23 @@ export default class ColumnCompApi extends Api {
         const res = await fetch(`${Api.getApiBaseUrl()}/column`);
         return res.json();
     }
+
+    static async createNewColumn(jsonService: {columnType: string; format: string; name: string}) {
+        const res = await fetch(`${Api.getApiBaseUrl()}/column/`, {method: "POST",
+        headers: {
+            "Content-Type": "application/json;charset=utf-8"
+        },
+            body: JSON.stringify(jsonService),
+        });
+        if(!res.ok) {
+            throw new Error(`Unable to create a new column with Attributes: ${JSON.stringify(jsonService)}`);
+        }
+    }
+
+    static async removeColumnById(id: number) {
+        const res = await fetch(`${Api.getApiBaseUrl()}/column/${id}`, {
+            method: "DELETE"
+        });
+        if(!res.ok) throw new Error(`Unable to delete column with id: ${id}`);
+    }
 }
