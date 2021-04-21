8c3e0d68229423c97be5e9cdac369a11d9262b8b
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-04-21 22:42:05 +0200

    Refactor Configs to use vue class component

--- src/views/Configs.vue
+++ src/views/Configs.vue
@@ -5,18 +5,16 @@
 <script lang="ts">
 import Vue from "vue";
 import ConfigEditor from "@/components/ConfigEditor.vue";
+import Component from "vue-class-component";
 
-export default Vue.extend({
-  name: "Configs",
-
+@Component({
   components: {
     ConfigEditor
   },
-
-  data: () => ({
-    title: "Configs",
-  }),
-});
+})
+export default class Configs extends Vue {
+  title = "Configs";
+}
 </script>
 
 <style scoped>
