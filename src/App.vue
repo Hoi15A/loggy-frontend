e7f825594d59117e0d6842c1b0f39baf9fc74133
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-04-25 15:29:04 +0200

    Redesign frontpage

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,131 @@
 <template>
   <v-app>
-    <v-card>
-      <v-navigation-drawer v-model="drawer" app>
-        <v-list-item class="px-2">
+    <v-navigation-drawer
+      app
+      clipped
+      v-model="drawer"
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>
+              mdi-view-dashboard
+            </v-icon>
+          </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title class="title">
-              Jane Doe
-            </v-list-item-title>
-            <v-list-item-subtitle>
-              Logged In
-            </v-list-item-subtitle>
-            
+            <v-list-item-title>Dashboard</v-list-item-title>
           </v-list-item-content>
-          <v-list-item-avatar>
-              <img src="https://randomuser.me/api/portraits/men/85.jpg">
-          </v-list-item-avatar>
         </v-list-item>
-        <v-divider></v-divider>
-        <v-list dense nav>
-          <v-list-item
-            v-for="item in items"
-            :key="item.title"
-            link
-            v-on:click="goTo(item.title)"
-          >
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
 
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+        app
+        elevation="1"
+        flat
+        clipped-left
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-img
+        src="https://i.imgur.com/9PKcAEa.png"
+        max-height="40px"
+        max-width="40px"
+        contain
+      >
+      </v-img>
+      <v-toolbar-title>
+        Loggy
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        icon
+      >
+        <v-icon size="26">
+          mdi-bell
+        </v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click="toggleTheme()"
+      >
+        <v-icon>
+          mdi-brightness-6
+        </v-icon>
+      </v-btn>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+              icon
+              v-on="on"
+              v-bind="attrs"
+          >
+            <v-avatar size="28">
+              <img src="https://randomuser.me/api/portraits/men/85.jpg">
+            </v-avatar>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-avatar size="28">
+                <img src="https://randomuser.me/api/portraits/men/85.jpg">
+              </v-avatar>
+            </v-list-item-icon>
+            <v-list-item-title>
+              John Doe
+            </v-list-item-title>
+          </v-list-item>
+          <v-divider></v-divider>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>
+                mdi-account-edit
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Edit Profile</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>
+                mdi-logout
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Logout</v-list-item-title>
           </v-list-item>
         </v-list>
-      </v-navigation-drawer>
-    </v-card>
-    <v-app-bar app absolute elevation="1" height="60">
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-btn x-large elevation="0" v-on:click="goHome()">Logy</v-btn>
-      <v-spacer></v-spacer>
-      <v-toolbar-title>{{title}}</v-toolbar-title>
-      <v-spacer></v-spacer>
+      </v-menu>
+
     </v-app-bar>
     <v-main>
       <v-container>
         <router-view/>
       </v-container>
     </v-main>
-    <v-footer app padless height="35">
-      <v-row class="align-center">
-        <v-col class="text-center" cols="12">
-          {{ new Date().getFullYear() }} — <strong>Log Analyser</strong>
+    <v-footer
+        flat
+        app
+        padless
+        outlined
+    >
+      <v-row
+        justify="center"
+        no-gutters
+      >
+        <v-btn
+          v-for="link in footerLinks"
+          :key="link"
+          text
+          color="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col
+          cols="12"
+          class="text-center"
+        >
+          {{ new Date().getFullYear()}} - <strong>Loggy</strong>
         </v-col>
+
       </v-row>
     </v-footer>
   </v-app>
@@ -77,8 +151,15 @@ export default Vue.extend({
         { title: "List Services", icon: "mdi-table"},
         { title: "Settings", icon: "mdi-cog"},
       ],
+      footerLinks: [
+        "Home",
+        "About Us",
+        "Team",
+        "Contact Us",
+      ],
       right: null,
       drawer: null,
+      panel: [0],
     };
   },
   methods: {
@@ -94,6 +175,9 @@ export default Vue.extend({
           this.$store.commit("stepper/setDialogStatus", true);
           break;
       }
+    },
+    toggleTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
     },
   },
 });
