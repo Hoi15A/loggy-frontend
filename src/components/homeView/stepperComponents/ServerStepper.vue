4e54187b656e1e99cddfa59436c8a33be98c87b3
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-04-27 14:35:09 +0200

    Refactor stepper to be vertical and replaced finish dialog with a snackbar

--- src/components/homeView/stepperComponents/ServerStepper.vue
+++ src/components/homeView/stepperComponents/ServerStepper.vue
@@ -1,65 +1,85 @@
 <template>
-  <v-stepper v-model="e1">
-    <v-stepper-header>
-      <v-stepper-step :complete="e1 > 1" step="1">
-        Initialize
-      </v-stepper-step>
-      <v-divider/>
-      <v-stepper-step :complete="e1 > 2" step="2">
-        Connect
-      </v-stepper-step>
-      <v-divider/>
-      <v-stepper-step step="3">
-        Finalize
-      </v-stepper-step>
-    </v-stepper-header>
+  <v-stepper
+    v-model="step"
+    vertical
+  >
+    <template>
+      <v-snackbar
+          v-model="snackbar"
+          :timeout="5000"
+      >
+        The service was successfully registered.
 
-    <!-- STEP 1 -->
-    <v-stepper-items>
-      <v-stepper-content step="1">
-        <UserInfoTextField ref="userForm"/>
-        <v-list-item elevation="0">
-          <CancelDialog v-on:confirmCancel="onConfirmCancel()"
-                        v-bind:button-name="buttonName"
-                        v-bind:title-message="titleMessage"/>
-          <v-spacer/>
-          <v-btn color="primary" rounded text @click="e1 = 2" v-on:click="onConfigSelect()" width="150">
-            Continue
+        <template v-slot:action="{ attrs }">
+          <v-btn
+              color="success"
+              text
+              v-bind="attrs"
+              @click="snackbar = false"
+          >
+            Close
           </v-btn>
-        </v-list-item>
-      </v-stepper-content>
+        </template>
+      </v-snackbar>
+    </template>
+   <!-- STEP 1 -->
+    <v-stepper-step 
+        :complete="step > 1" 
+        step="1"
+    >
+      Initialize
+    </v-stepper-step>
+    <v-stepper-content step="1">
+      <UserInfoTextField ref="userForm"/>
+      <v-list-item elevation="0">
+        <CancelDialog v-on:confirmCancel="onConfirmCancel()"
+                      v-bind:button-name="buttonName"
+                      v-bind:title-message="titleMessage"/>
+        <v-spacer/>
+        <v-btn color="primary" rounded text @click="step = 2" v-on:click="onConfigSelect()" width="150">
+          Continue
+        </v-btn>
+      </v-list-item>
+    </v-stepper-content>
 
-      <!-- STEP 2 -->
-      <v-stepper-content step="2">
-        <ConfigSelectForm ref="configForm"/>
-        <v-list-item elevation="0">
-          <CancelDialog v-on:confirmCancel="onConfirmCancel()"
-                        v-bind:button-name="buttonName"
-                        v-bind:title-message="titleMessage"/>
-          <v-spacer/>
-          <v-btn color="grey" dark rounded text @click="e1 = 1">Back</v-btn>
-          <v-spacer/>
-          <v-btn color="primary" rounded text @click="e1 = 3" width="150">Continue</v-btn>
-        </v-list-item>
-      </v-stepper-content>
+    <!-- STEP 2 -->
+    <v-stepper-step 
+        :complete="step > 2" 
+        step="2"
+    >
+      Connect
+    </v-stepper-step>
+    <v-stepper-content step="2">
+      <ConfigSelectForm ref="configForm"/>
+      <v-list-item elevation="0">
+        <CancelDialog v-on:confirmCancel="onConfirmCancel()"
+                      v-bind:button-name="buttonName"
+                      v-bind:title-message="titleMessage"/>
+        <v-spacer/>
+        <v-btn color="grey" dark rounded text @click="step = 1">Back</v-btn>
+        <v-spacer/>
+        <v-btn color="primary" rounded text @click="step = 3" width="150">Continue</v-btn>
+      </v-list-item>
+    </v-stepper-content>
 
-      <!-- STEP 3 -->
-      <v-stepper-content step="3">
-        <DirectoryLocationForm ref="directoryLocationForm"/>
-        <v-list-item elevation="0">
-          <CancelDialog v-on:confirmCancel="onConfirmCancel()"
-                        v-bind:button-name="buttonName"
-                        v-bind:title-message="titleMessage"/>
-          <v-spacer/>
-          <v-btn color="grey" rounded text @click="e1 = 2">Back</v-btn>
-          <v-spacer/>
-          <v-btn color="green" dark rounded text v-on:click="onClickDone()" width="150">Finish</v-btn>
-        </v-list-item>
-      </v-stepper-content>
-    </v-stepper-items>
-    <ProcessingDialog ref="processingDialog" v-on:confirmSuccess="onConfirmSuccess()"
-                      v-on:cancelFailure="onCancelFailure()"/>
+    <!-- STEP 3 -->
+    <v-stepper-step step="3">
+      Finalize
+    </v-stepper-step>
+    <v-stepper-content step="3">
+      <DirectoryLocationForm ref="directoryLocationForm"/>
+      <v-list-item elevation="0">
+        <CancelDialog v-on:confirmCancel="onConfirmCancel()"
+                      v-bind:button-name="buttonName"
+                      v-bind:title-message="titleMessage"/>
+        <v-spacer/>
+        <v-btn color="grey" rounded text @click="step = 2">Back</v-btn>
+        <v-spacer/>
+        <v-btn color="green" dark rounded text v-on:click="onClickDone()" width="150">Finish</v-btn>
+      </v-list-item>
+    </v-stepper-content>
   </v-stepper>
+
 </template>
 
 <script lang="js">
@@ -67,7 +87,6 @@ import Vue from "vue";
 import CancelDialog from "@/components/CancelDialog";
 import UserInfoTextField from "@/components/homeView/stepperComponents/UserInfoForm";
 import ConfigSelectForm from "@/components/homeView/stepperComponents/ConfigSelectForm";
-import ProcessingDialog from "@/components/homeView/stepperComponents/ProcessingDialog";
 import serviceApi from "@/api/serviceApi";
 import DirectoryLocationForm from "@/components/homeView/stepperComponents/DirectoryLocationForm";
 
@@ -77,28 +96,18 @@ export default Vue.extend({
     CancelDialog,
     ConfigSelectForm,
     DirectoryLocationForm,
-    ProcessingDialog,
   },
 
   data: () => ({
+    server: {},
     buttonName: "Cancel",
     titleMessage: "Are you sure you want to cancel the registration process?",
-    e1: 1,
-
-    server: {
-      logDirectory: "",
-      name: "",
-      description: "",
-      image: "",
-      location: 1,
-      logConfig: ""
-    }
+    step: 1,
+    snackbar: false,
   }),
 
   methods: {
-    async onClickDone() {
-      this.$refs.processingDialog.activateProcessing();
-
+    onClickDone() {
       this.server.name = this.$refs.userForm.name;
       this.server.image = this.$refs.userForm.image;
       this.server.description = this.$refs.userForm.description;
@@ -106,26 +115,24 @@ export default Vue.extend({
       this.server.logDirectory = this.$refs.directoryLocationForm.selection[0].fullpath;
       this.server.location = this.$refs.directoryLocationForm.location;
 
-      try {
-        await serviceApi.addNewService(this.server);
-        this.$refs.processingDialog.activateSuccess();
-      } catch (e) {
-        this.$refs.processingDialog.activateFailure(e.toString());
-      }
+      serviceApi.addNewService(this.server)
+        .then(() => {
+          this.snackbar = true;
+          this.server = {};
+          this.step = 1;
+        })
+        .catch(err => {
+          console.error(err);
+        });
     },
 
     onConfirmCancel() {
-      this.e1 = 1;
+      this.step = 1;
       this.$emit("stepperCancel");
     },
 
-    onConfirmSuccess() {
-      this.e1 = 1;
-      this.$emit("stepperComplete");
-    },
-
     onCancelFailure() {
-      this.e1 = 1;
+      this.step = 1;
     },
 
     onConfigSelect() {
