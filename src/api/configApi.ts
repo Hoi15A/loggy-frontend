ec515b2f63b64d2d3c8c4b48f4634c25b29fa241
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-04-27 13:49:17 +0200

    Add remove and update config methods

--- src/api/configApi.ts
+++ src/api/configApi.ts
@@ -1,4 +1,5 @@
 import Api from "@/api/api";
+import {Config} from "@/models/config";
 
 export default class ConfigApi extends Api {
   constructor() {
@@ -12,6 +13,16 @@ export default class ConfigApi extends Api {
 
   static async fetchAllConfigs() {
     const res =  await fetch(`${this.apiBaseUrl}/config`);
+    return res.json();
+  }
+
+  static async removeConfigById(name: string) {
+    const res = await fetch(`${this.apiBaseUrl}/config/${name}`, {method: "DELETE"});
+    return res.json();
+  }
+
+  static async updateConfig(config: Config) {
+    const res = await fetch(`${this.apiBaseUrl}/config/`, {method: "PUT", body: JSON.stringify(config)});
     return res.json();
   }
 
