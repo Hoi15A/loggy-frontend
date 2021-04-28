ac8837a6338ff3e4b439477928dacfa5f8ab3715
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-04-28 22:36:16 +0200

    Refactor ColumnComponentsReorder

--- src/components/configViewComponents/ColumnComponentsReorder.vue
+++ src/components/configViewComponents/ColumnComponentsReorder.vue
@@ -1,17 +1,22 @@
 <template>
     <v-data-table
         :headers="columnHeaders"
-        :items="columnData"
+        :items="Object.values($store.getters['config/getConfigById'](this.configName).columnComponents)"
         hide-default-footer
     >
       <template v-slot:body="props">
-        <draggable :list="props.items" tag="tbody">
-          <tr v-for="(entry, index) in props.items" :key="index" v-bind:style="{cursor: 'move'}">
+        <draggable
+            :list="props.items"
+            tag="tbody"
+        >
+          <tr v-for="entry in props.items"
+              :key="entry.id"
+              v-bind:style="{cursor: 'move'}"
+          >
             <td> {{ entry.id }} </td>
             <td> {{ entry.name }} </td>
             <td> {{ entry.format }} </td>
             <td> {{ entry.columnType }} </td>
-
           </tr>
         </draggable>
       </template>
@@ -23,10 +28,6 @@ import Vue from "vue";
 import {Prop, Component} from "vue-property-decorator";
 import "vue-class-component/hooks";
 import Draggable from "vuedraggable";
-import {ColumnComponent} from "@/models/columnComponent";
-import {Config} from "@/models/config";
-
-
 
 @Component({
   components: {
@@ -36,24 +37,12 @@ import {Config} from "@/models/config";
 export default class ColumnComponentsReorder extends Vue {
   @Prop(String) configName: string | undefined
 
-  columnHeaders = [] as object[];
-  columnData = [] as ColumnComponent[];
-  serviceConfig = {} as Config;
-
-  created() {
-    this.parseConfigs();
-    this.fillHeader();
-  }
-
-  fillHeader() {
-    this.columnHeaders.push({text: "ID"}, {text: "NAME"}, {text: "FORMAT"}, {text: "COLUMNTYPE"},);
-  }
-
-  parseConfigs() {
-    this.serviceConfig = this.$store.getters["config/getConfigById"](this.configName);
-    this.columnData = Object.values(this.serviceConfig.columnComponents);
-  }
-
+  columnHeaders = [
+    { text: "ID", value: "id"},
+    { text: "Name", value: "name"},
+    { text: "Format", value: "format"},
+    { text: "Column Type", value: "columnType"},
+  ];
 }
 
 </script>
