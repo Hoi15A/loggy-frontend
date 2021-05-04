900c7f4a0889c1a4f0502ee2af421a031c1b70e2
Carlos Olgiati <olgiacar@students.zhaw.ch>
2021-05-04 15:23:09 +0200

    Cleaned Test.vue

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -5,7 +5,6 @@
 <script lang="ts">
 import Vue from "vue";
 import DateFilter from "@/components/serverView/filterInput/DateFilter.vue";
-import { ColumnDefinition } from "@/models/columnDefinition";
 
 export default Vue.extend({
   name: "Test",
@@ -13,23 +12,5 @@ export default Vue.extend({
   components: {
     DateFilter
   },
-
-  data: () => ({
-    columnDefinitions: [] as ColumnDefinition[],
-    rowData: null,
-  }),
-
-  beforeMount() {
-    this.columnDefinitions = [
-      {headerName: "Make", field: "make", resizable: true, sortable: true, filter: true},
-      {headerName: "Model", field: "model", resizable: true, sortable: true, filter: true},
-      {headerName: "Price", field: "price", resizable: true, sortable: true, filter: true}
-    ];
-
-    fetch("https://www.ag-grid.com/example-assets/row-data.json")
-      .then(result => result.json())
-      .then(rowData => this.rowData = rowData);
-  },
-
 });
 </script>
