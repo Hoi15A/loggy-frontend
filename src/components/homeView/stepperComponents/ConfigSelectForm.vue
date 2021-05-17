014fff0c4f30e364377438cbc01a656f07ec625d
schwaal3 <schwaal3@students.zhaw.ch>
2021-05-17 23:17:21 +0200

    changed import to case sensitive file name

--- src/components/homeView/stepperComponents/ConfigSelectForm.vue
+++ src/components/homeView/stepperComponents/ConfigSelectForm.vue
@@ -2,7 +2,7 @@
   <v-form>
     <v-container>
       <v-row>
-        <v-select :items="items" label="Config" v-model="selectedConfig"/>
+        <v-select :items="items" label="Config" v-model="setConfigName"/>
         <v-spacer/>
         <NewConfigSideWindow v-on:newConfigSuccess="loadConfigs()"/>
       </v-row>
@@ -15,6 +15,7 @@ import Vue from "vue";
 import ServiceApi from "@/api/serviceApi";
 import NewConfigSideWindow from "@/components/homeView/stepperComponents/NewConfigSideWindow.vue";
 import {Component} from "vue-property-decorator";
+import {Server} from "@/models/server";
 
 @Component({
   components: {
@@ -24,11 +25,16 @@ import {Component} from "vue-property-decorator";
 export default class ConfigSelectForm extends Vue {
   items = [] as string[];
   cardVisible = false;
-  selectedConfig = undefined;
 
   async loadConfigs() {
     const fetchedConfigs = await ServiceApi.fetchConfigs();
     fetchedConfigs.forEach(config => this.items.push(config.name));
+  }
+
+  set setConfigName(value: string) {
+    const server = this.$store.getters["stepper/getServer"] as Server;
+    server["logConfig"] = value;
+    this.$store.commit("stepper/setServer", server);
   }
 
   onOpenCard() {
