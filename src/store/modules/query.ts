c77a19ffd9869bc25567522955976b7baba73ed1
pedernin <pedernin@students.zhaw.ch>
2021-05-19 18:00:01 +0200

    replaced [string, string] with object, so that we can add the dateFormat for the DateFilter Components #27

--- src/store/modules/query.ts
+++ src/store/modules/query.ts
@@ -2,16 +2,22 @@ import {Module, Mutation, VuexModule} from "vuex-module-decorators";
 import store from "@/store";
 
 interface QueryInterface {
-  [key: string]: string | [string, string];
+  [key: string]: string | {};
 }
 @Module({store, name: "querystore", dynamic: true})
 export default class QueryStore extends VuexModule {
   queries = {} as QueryInterface;
 
   @Mutation
-  addQuery(values: [string, string] | [string, [string, string]]) {
+  addQuery(values: [string, string]) {
     this.queries[values[0]] = values[1];
   }
+
+  @Mutation
+  addRangeQuery(values: [string, {}]) {
+    this.queries[values[0]] = values[1];
+  }
+
 
   get getAllQueries() {
     return this.queries;
