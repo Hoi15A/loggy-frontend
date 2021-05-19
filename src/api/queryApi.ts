3154e8dc028ab4ebb283cd69df4d82b42229c5cf
pedernin <pedernin@students.zhaw.ch>
2021-05-19 17:55:52 +0200

    added body to POST in query api #27

--- src/api/queryApi.ts
+++ src/api/queryApi.ts
@@ -1,12 +1,14 @@
 import Api from "./api";
 
-export default class QueryApi extends Api { 
-  static async query(id: number) {
-    return (await fetch(`${this.apiBaseUrl}/query/${id}`, {
+export default class QueryApi extends Api {
+
+  static async query(id: number, queries: any []) {
+    return await fetch(`${this.apiBaseUrl}/query/${id}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
-      }
-    })).json();
+      },
+      body: JSON.stringify(queries),
+    });
   }
 }
