beabb91cd19deae453f782e6924166f16490a61f
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-05-25 13:31:27 +0200

    add validation for user info form

--- src/components/homeView/stepperComponents/UserInfoForm.vue
+++ src/components/homeView/stepperComponents/UserInfoForm.vue
@@ -1,22 +1,48 @@
 <template>
+  <ValidationObserver>
   <v-form>
     <v-container>
       <v-row align="center">
         <v-col>
-          <v-text-field counter label="Service Name" maxlength="30" v-model="name"/>
+          <ValidationProvider
+            rules="required|max:30"
+            v-slot="{ errors }"
+            name="service name"
+          >
+          <v-text-field 
+            counter="30"
+            label="Service Name" 
+            :error-messages="errors" 
+            v-model="name"
+          />
+          </ValidationProvider >
         </v-col>
         <v-col>
-          <v-text-field label="Icon Link" maxlength="200" v-model="image"/>
+          <v-text-field label="Icon Link" v-model="image"/>
         </v-col>
       </v-row>
       <v-row>
         <v-col>
-          <v-textarea counter maxlength="200" no-resize rows="3" outlined name="input-7-4"
-                      label="Description" v-model="description"/>
+          <ValidationProvider
+            rules="max:200"
+            v-slot="{ errors }"
+            name="description"
+          >
+          <v-textarea 
+            counter="200" 
+            rows="3" 
+            outlined 
+            name="input-7-4"
+            label="Description" 
+            v-model="description"
+            :error-messages="errors"
+          />
+          </ValidationProvider>
         </v-col>
       </v-row>
     </v-container>
   </v-form>
+  </ValidationObserver>
 </template>
 
 <script lang="ts">
